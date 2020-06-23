a449f04ee1ec993cff8b7154690d6a3cb07916a1
MirageTamanaha <65757877+MirageTamanaha@users.noreply.github.com>
2020-06-23 16:23:32 +0900

    やった

--- Megumirai/Controllers/LoginController.cs
+++ Megumirai/Controllers/LoginController.cs
@@ -9,24 +9,32 @@ using Megumirai.Models;
 
 namespace Megumirai.Controllers
 {
-
+    [Authorize]
     public class LoginController : Controller
     {
-       [HttpGet]
+        private Database1Entities1 db = new Database1Entities1();
+
+        [AllowAnonymous]
         public ActionResult EmpLoginIndex()
         {
+            if (Request.IsAuthenticated)
+            {
+                return View("EmpMainMenu");
+            }
             return View();
         }
+        [AllowAnonymous]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult EmpLoginIndex(EmpLoginModel model)
         {
             using (var db = new Database1Entities1())
             {
                 var ul = db.Employees.Find(int.Parse(model.EmployeeId));
-                if (int.Parse(model.EmployeeId)==ul.EmployeeId && model.Password==ul.Password)
+                if (int.Parse(model.EmployeeId) == ul.EmployeeId && model.Password == ul.Password)
                 {
-                    // ユーザー認証 成功    
-                    FormsAuthentication.SetAuthCookie(model.EmployeeId, model.RememberMe);
+                    // ユーザー認証 成功 
+                    FormsAuthentication.SetAuthCookie(userName: model.EmployeeId, createPersistentCookie: false);
                     return this.Redirect("/Login/EmpMainMenu");
                 }
                 else
@@ -36,19 +44,66 @@ namespace Megumirai.Controllers
                     return this.View(model);
                 }
             }
-           }
+        }
         public ActionResult EmpMainMenu()
         {
             return View();
         }
-        [HttpPost]
         public ActionResult EmpLogout()
         {
             FormsAuthentication.SignOut();
             return View("EmpLoginIndex");
         }
+        [AllowAnonymous]
+        public ActionResult CsLoginIndex()
+        {
+            if (Request.IsAuthenticated)
+            {
+                return View("CsMainMenu");
+            }
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CsLoginIndex(CsLoginModel model, int customerid)
+        {
+            using (var db = new Database1Entities1())
+            {
+                var ul = db.Customers.Find(int.Parse(model.CustomerId));
+                if (int.Parse(model.CustomerId) == ul.CustomerId && model.Password == ul.Password)
+                {
+                    var CsSession = new Customer()
+                    {
+                        CustomerId = customerid
+                    };
+                    Session["cslogin"] = CsSession;
+                    FormsAuthentication.SetAuthCookie(userName:model.CustomerId, createPersistentCookie: false);
+
+                    // ユーザー認証 成功
+                    return this.Redirect("/Login/CsMainMenu");
+                }
+                else
+                {
+                    // ユーザー認証 失敗
+                    ModelState.AddModelError(string.Empty, "入力された顧客IDまたはパスワードが誤っています");
+                    return View(model);
+                }
+            }
+        }
+        public ActionResult CsMainMenu()
+        {
+            return View();
+        } 
+        public ActionResult CsLogout()
+        {
+            Session.Abandon();
+            FormsAuthentication.SignOut();
+            return this.Redirect("/");
+        }
     }
-    }
+}
+    
         
     
 
---
+++ Megumirai/Models/CsLoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Megumirai.Models
+{
+    public class CsLoginModel
+    {
+        
+        [Required(ErrorMessage = "顧客ID（パスワード）が入力されていません")]
+        [RegularExpression("[0-9]{6}", ErrorMessage = "入力された顧客IDまたはパスワードが誤っています")]
+        public string CustomerId { get; set; }
+        [Required(ErrorMessage = "顧客ID（パスワード）が入力されていません")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
